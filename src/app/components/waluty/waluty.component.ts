0be91f77a2698284984d97a752743ecab46fa988
obelixx1984 <karbowiakk@wp.pl>
2022-03-31 23:21:44 +0200

    przeniesienie danych do komponentu "waluty"

--- src/app/components/waluty/waluty.component.ts
+++ src/app/components/waluty/waluty.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { BazaService, TabelaNBP, Wskazniki } from 'src/app/_services/baza.service';
 
 @Component({
   selector: 'waluty',
@@ -6,10 +7,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./waluty.component.css']
 })
 export class WalutyComponent implements OnInit {
+  tabelanbp: Array<TabelaNBP> = [];
 
-  constructor() { }
+  pelnaLista: Array<Wskazniki> = [];
+  krotkaLista: Array<Wskazniki> = [];
+  ostatecznaLista: Array<Wskazniki> = [];
 
-  ngOnInit(): void {
+  pktWiecej = false;
+  btnWiecej = 'Załaduj więcej';
+
+  classFlags='currency-flag currency-flag-';
+
+  constructor(private bazaService: BazaService) { }
+
+  ngOnInit(): void { this.uruchomListeWalut(); }
+
+  uruchomListeWalut(): void
+  {
+    this.bazaService.urlNBPTabelaA().subscribe( przedmiot => {
+     this.tabelanbp = przedmiot;
+     this.tabelanbp[0].rates.forEach( (wskaznik) => {
+        this.pelnaLista.push(wskaznik);
+     });
+     this.krotkaLista.push(this.pelnaLista[1]);
+     this.krotkaLista.push(this.pelnaLista[7]);
+     this.krotkaLista.push(this.pelnaLista[10]);
+     this.krotkaLista.push(this.pelnaLista[9]);
+     this.krotkaLista.push(this.pelnaLista[13]);
+     this.krotkaLista.push(this.pelnaLista[32]);
+     this.krotkaLista.push(this.pelnaLista[12]);
+     this.krotkaLista.push(this.pelnaLista[2]);
+     this.ostatecznaLista = this.krotkaLista;
+     console.log(this.krotkaLista[0]);
+    });
   }
 
+  zaladujWiecej(): void {
+    if (this.pktWiecej)
+    {
+      this.ostatecznaLista = this.krotkaLista;
+      this.btnWiecej = 'Załaduj więcej';
+      this.pktWiecej = false;
+    }
+    else
+    {
+      this.ostatecznaLista = this.pelnaLista;
+      this.btnWiecej = 'Pokaż mniej';
+      this.pktWiecej = true;
+    }
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BazaService, TabelaNBP, Wskazniki } from '../_services/baza.service';
 
 @Component({
   selector: 'app-home',
@@ -7,53 +6,9 @@ import { BazaService, TabelaNBP, Wskazniki } from '../_services/baza.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  tabelanbp: Array<TabelaNBP> = [];
 
-  pelnaLista: Array<Wskazniki> = [];
-  krotkaLista: Array<Wskazniki> = [];
-  ostatecznaLista: Array<Wskazniki> = [];
+  constructor() { }
 
-  pktWiecej = false;
-  btnWiecej = 'Załaduj więcej';
+  ngOnInit(): void { }
 
-  classFlags='currency-flag currency-flag-';
-
-  constructor(private bazaService: BazaService) { }
-
-  ngOnInit(): void { this.uruchomListeWalut(); }
-
-  uruchomListeWalut(): void
-  {
-    this.bazaService.urlNBPTabelaA().subscribe( przedmiot => {
-     this.tabelanbp = przedmiot;
-     this.tabelanbp[0].rates.forEach( (wskaznik) => {
-        this.pelnaLista.push(wskaznik);
-     });
-     this.krotkaLista.push(this.pelnaLista[1]);
-     this.krotkaLista.push(this.pelnaLista[7]);
-     this.krotkaLista.push(this.pelnaLista[10]);
-     this.krotkaLista.push(this.pelnaLista[9]);
-     this.krotkaLista.push(this.pelnaLista[13]);
-     this.krotkaLista.push(this.pelnaLista[32]);
-     this.krotkaLista.push(this.pelnaLista[12]);
-     this.krotkaLista.push(this.pelnaLista[2]);
-     this.ostatecznaLista = this.krotkaLista;
-     console.log(this.krotkaLista[0]);
-    });
-  }
-
-  zaladujWiecej(): void {
-    if (this.pktWiecej)
-    {
-      this.ostatecznaLista = this.krotkaLista;
-      this.btnWiecej = 'Załaduj więcej';
-      this.pktWiecej = false;
-    }
-    else
-    {
-      this.ostatecznaLista = this.pelnaLista;
-      this.btnWiecej = 'Pokaż mniej';
-      this.pktWiecej = true;
-    }
-  }
 }
