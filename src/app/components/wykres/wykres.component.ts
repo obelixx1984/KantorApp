e9e90a2125d845daca9c22741626b16018406402
obelixx1984 <karbowiakk@wp.pl>
2022-04-05 22:13:47 +0200

    pokazanie danych walut na wykresach

--- src/app/components/wykres/wykres.component.ts
+++ src/app/components/wykres/wykres.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { ChartType } from 'chart.js';
-import { BazaService } from 'src/app/_services/baza.service';
+import * as moment from 'moment';
+import { BazaService, Wskazniki } from 'src/app/_services/baza.service';
 
 @Component({
   selector: 'wykres',
@@ -8,28 +9,66 @@ import { BazaService } from 'src/app/_services/baza.service';
   styleUrls: ['./wykres.component.css']
 })
 export class WykresComponent implements OnInit {
-  private nbpFormatDaty = 'YYYY-MM-DD';
-  
-  public barChartOptions = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };  
-  
-  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  
-  public barChartType: ChartType = 'line';
-  
-  public barChartLegend = true;  
-  
-  public barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-  ];
+  @Input() kodwaluty: any;
 
+  nbpFormatDaty = 'YYYY-MM-DD';
 
+  public typWykresu: ChartType = 'line';
+
+  public daneWykresu: Array<any> = [];
+  public liniaXWykresu: Array<any> = [];
+
+  public opcjeWykresu = 
+    {
+      borderColor: '#3f5118',
+      borderWidth: 1,
+      scaleShowVerticalLines: false,
+      responsive: true
+    };
+
+  public legendaWykresu = false; 
+  
+  public nazwaWaluty: Array<any> = [];
+
+  public dataOd: any;
+  public dataDo: any;
+  public teraz: any;
+  public kurs = 'eur';
+  
   constructor(private bazaService: BazaService) { }
 
   ngOnInit(): void {
+    this.inicjujDate();
+    this.inicjujWykres();
+  }
+
+  inicjujDate(): void 
+  {
+    this.teraz = moment();
+    this.dataDo = this.teraz.format(this.nbpFormatDaty);
+    this.dataOd = this.teraz.subtract(31, 'days').format(this.nbpFormatDaty);
+  }
+
+  inicjujWykres(): void
+  {
+    this.bazaService.urlNBPTabelaAHistoria(this.kodwaluty.code, this.dataOd, this.dataDo).subscribe(
+      (rezultat) => {
+        this.wczytajdaneWykresu(rezultat);
+      }
+    );
+  }
+
+  wczytajdaneWykresu(rezultat: any): void
+  {
+    let danezURL: Array<number> = new Array<number>();
+    let daneURLXwykresu: Array<string> = new Array<string>();
+    rezultat.rates.forEach((elementH: any) => {
+      danezURL.push(elementH.mid);
+      daneURLXwykresu.push(elementH.effectiveDate);
+    });
+    this.daneWykresu = [];
+    this.daneWykresu.push({data: danezURL, label: 'Kurs ' + this.kodwaluty.code.toUpperCase()});
+    this.liniaXWykresu = daneURLXwykresu;
   }
 
 }
